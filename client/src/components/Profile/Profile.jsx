d43488a9165446ece3cc40dce76cd5395d29c99a
Rhythm Shandlya <armaanbgp@gmail.com>
2022-07-01 23:03:21 +0530

    log

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -2,11 +2,12 @@ import useFetch from "../../hooks/useFetch";
 import Loading from "../Loading/Loading";
 
 const Profile = () => {
-  const { data, loading } = useFetch("/auth/me");
+  const { data, loading, error } = useFetch("/auth/me");
   return loading ? (
     <Loading />
   ) : (
     <>
+      <p></p>
       <p>Name: {data?.user?.name}</p>
       <p>Email: {data?.user?.email}</p>
       <p>Role: {data?.user?.role}</p>
