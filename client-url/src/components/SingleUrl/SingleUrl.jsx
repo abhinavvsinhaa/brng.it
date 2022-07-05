dcab26848c4cab4532172409d6b3984b8a5294af
imOmsingh <94113510+imOmsingh@users.noreply.github.com>
2022-07-05 21:01:53 +0530

    Add files via upload

---
+++ client-url/src/components/SingleUrl/SingleUrl.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function SingleUrl({varArr}) {
+  return (<>
+        {
+          varArr.shortUrl ? <Link to={`/url/${varArr.uid}`} target='_blank' >{varArr.shortUrl}</Link> :
+          <Link to={`/url/${varArr.uid}`} target='_blank' >{varArr.short}</Link>
+        }
+        <br/>
+
+    </>
+    )
+}
