32ef1bef7d5ee5564b5787c4c06100cb84f8d4ed
Chitwan Bindal <chitwan001@gmail.com>
2022-08-13 20:23:21 +0530

    download app working

--- client/src/components/WiseStamp/components/Apps/DownloadApp.jsx
+++ client/src/components/WiseStamp/components/Apps/DownloadApp.jsx
@@ -1,34 +1,90 @@
 import { useState } from "react"
 import googleplay from '../../../../assets/icons/google-play-badge.png';
 import appstore from '../../../../assets/icons/app-store-badge.png';
+import { ChromePicker } from "react-color";
+import { Droplet } from "@styled-icons/boxicons-solid";
+import { AlignCenter, AlignLeft, AlignRight } from "@styled-icons/feather";
 export default function DownloadApp(){
     const [buttonSize,setbuttonSize] = useState('20px')
-    const [fontColor,setfontColor] = useState('#fff')
-    const [fontSize,setfontSize] = useState('10px')
-    const [alignment,setalignment] = useState('10px')
+    const [fontColor,setfontColor] = useState('#000')
+    const [fontSize,setfontSize] = useState('10')
+    const [alignment,setalignment] = useState('l')
     const [title,setTitle] = useState('Download our app')
     const [playLink,setplayLink] = useState('https://play.google.com/store/apps/details?id=APP_ID')
     const [appStoreLink,setappStoreLink] = useState('https://itunes.apple.com/us/app/APP_NAME')
+    const [fontcolorDiv,setfontcolorDiv] = useState(false)
 
     return(
         <div className="grid grid-cols-[1fr_1.5fr] gap-[10px] h-full pl-[30px]">
-            <div className="grid grid-rows-[1fr_5fr_7fr]">
+            <div className="grid grid-rows-[1fr_auto_7fr]">
                 <div className="grid">
                     <div className="font-bold pl-[5px] text-[30px] tracking-wide grid content-center">Download our app</div>
                 </div>
-                <div></div>
-                <div className="grid grid-rows-[50px_50px_50px] gap-[10px]">
-                    <div className="grid grid-cols-[170px_auto]">
-                        <div className="grid tracking-wide text-[16px] pl-[5px] items-center">Font Color</div>
-                        <input className="outline-0 pl-[5px] border-[#a9a9a9] border-b-[1px]" type="text" name="signname" id="signname" />
+                <div className="grid grid-rows-[40px_auto] gap-[10px] pl-[10px]">
+                    <div className="font-bold text-[18px]">Button Details</div>
+                    <div className="grid grid-rows-[50px_50px_50px] gap-[10px]">
+                        <div className="grid grid-cols-[100px_1fr] gap-[10px]">
+                            <div className="grid place-content-center text-[14px]">Title</div>
+                            <div className="grid">
+                                <input value={title} className="outline-0 pl-[10px] border-[#a9a9a9] border-b-[1px]" type='text' onChange={(e)=>{setTitle(e.target.value)}}/>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-[100px_1fr] gap-[10px]">
+                            <div className="grid place-content-center text-[14px]">Google Play</div>
+                            <div className="grid">
+                                <input value={playLink} className="outline-0 pl-[10px] border-[#a9a9a9] border-b-[1px]" type='text' onChange={(e)=>{setplayLink(e.target.value)}}/>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-[100px_1fr] gap-[10px]">
+                            <div className="grid place-content-center text-[14px]">App store</div>
+                            <div className="grid">
+                                <input value={appStoreLink} className="outline-0 pl-[10px] border-[#a9a9a9] border-b-[1px]" type='text' onChange={(e)=>{setappStoreLink(e.target.value)}}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid grid-rows-[50px_50px_50px_50px] gap-[10px] pt-[30px]">
+                    <div className="font-bold text-[20px] pl-[10px]">Style</div>
+                <div className="grid grid-cols-[170px_auto]">
+                        <div className="grid tracking-wide text-[16px] pl-[5px] items-center">Color</div>
+                        <div className="grid grid-cols-[auto_1fr] w-fit place-self-center gap-[10px]">
+                            <div className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr] gap-[5px]">
+                                <div onClick={() => {setfontColor('#000')}} className={`cursor-pointer ${fontColor==="#000"?'border-blue-500 border-2':''} rounded-full w-[20px] h-[20px] place-self-center bg-black`}></div>
+                                <div onClick={() => {setfontColor('#9158BD')}} className={`cursor-pointer ${fontColor==="#9158BD"?'border-blue-500 border-2':''} rounded-full w-[20px] h-[20px] place-self-center bg-[#9158BD]`}></div>
+                                <div onClick={() => {setfontColor('#DE627B')}} className={`cursor-pointer ${fontColor==="#DE627B"?'border-blue-500 border-2':''} rounded-full w-[20px] h-[20px] place-self-center bg-[#DE627B]`}></div>
+                                <div onClick={() => {setfontColor('#1BA2EB')}} className={`cursor-pointer ${fontColor==="#1BA2EB"?'border-blue-500 border-2':''} rounded-full w-[20px] h-[20px] place-self-center bg-[#1BA2EB]`}></div>
+                                <div onClick={() => {setfontColor('#53B700')}} className={`cursor-pointer ${fontColor==="#53B700"?'border-blue-500 border-2':''} rounded-full w-[20px] h-[20px] place-self-center bg-[#53B700]`}></div>
+                                <div onClick={() => {setfontColor('#EEBE36')}} className={`cursor-pointer ${fontColor==="#EEBE36"?'border-blue-500 border-2':''} rounded-full w-[20px] h-[20px] place-self-center bg-[#EEBE36]`}></div>
+                                <div onClick={() => {setfontColor('#FF6009')}} className={`cursor-pointer ${fontColor==="#FF6009"?'border-blue-500 border-2':''} rounded-full w-[20px] h-[20px] place-self-center bg-[#FF6009]`}></div>
+                                <div onClick={() => {setfontColor('#B92424')}} className={`cursor-pointer ${fontColor==="#B92424"?'border-blue-500 border-2':''} rounded-full w-[20px] h-[20px] place-self-center bg-[#B92424]`}></div>
+                            </div>
+                            <div className="relative grid place-content-center">
+                                <div onClick={() => {setfontcolorDiv(!fontcolorDiv)}} className="p-[1px] w-[30px] h-[30px] grid place-content-center rounded-full border-[1.5px] cursor-pointer border-black">
+                                    <Droplet width='20px'/>
+                                </div>
+                                <div className="absolute top-full -left-[90%]">
+                                    {fontcolorDiv?
+                                    (<ChromePicker color={fontColor} onChangeComplete={(color) => {setfontColor(color.hex)}}/>)
+                                    :
+                                    ''
+                                    }
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="grid grid-cols-[170px_auto]">
                         <div className="grid tracking-wide text-[16px] pl-[5px] items-center">Font Size</div>
-                        <input className="outline-0 pl-[5px] border-[#a9a9a9] border-b-[1px]" type="text" name="signname" id="signname" />
+                        <div className="grid place-self-center w-[80%]">
+                            <input type="range" name="size" id="size" onChange={(e)=>{setfontSize(e.target.value)}} value={fontSize} min='8' max='17' />
+                        </div>
                     </div>
                     <div className="grid grid-cols-[170px_auto]">
                         <div className="grid tracking-wide text-[16px] pl-[5px] items-center">Alignment</div>
-                        <input className="outline-0 pl-[5px] border-[#a9a9a9] border-b-[1px]" type="text" name="signname" id="signname" />
+                        <div className="grid grid-flow-col gap-[10px] w-fit">
+                            <div className={`${alignment==='l'?'bg-black text-white':''} w-[50px] grid place-content-center border border-black text-black`} onClick={()=>{setalignment('l')}}><AlignLeft width='40px'/></div>
+                            <div className={`${alignment==='m'?'bg-black text-white':''} w-[50px] grid place-content-center border border-black text-black`} onClick={()=>{setalignment('m')}}><AlignCenter width='40px'/></div>
+                            <div className={`${alignment==='r'?'bg-black text-white':''} w-[50px] grid place-content-center border border-black text-black`} onClick={()=>{setalignment('r')}}><AlignRight width='40px'/></div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -37,7 +93,7 @@ export default function DownloadApp(){
                     <div className="overflow-hidden rounded-lg w-[695px] grid grid-rows-[26px_1fr] h-[200px] shadow-[rgba(0,_0,_0,_0.02)_0px_1px_3px_0px,_rgba(27,_31,_35,_0.15)_0px_0px_0px_1px] bg-[#f8f9fb]">
                         <div className="bg-[#292c33] text-white pl-[8px] py-[3px]">New Message</div>
                         <div className="pl-[10px] pt-[10px]">
-                            <div style={{'fontSize':`${fontSize}`,'letterSpacing':'1.5px'}}>Download our app</div>
+                            <div style={{'fontSize':`${fontSize}px`,'letterSpacing':'1.5px','color':fontColor,'textAlign':`${alignment==='l'?'left':alignment==='m'?'center':'right'}`}}>{title}</div>
                             <div style={{padding:'10px 0px','display':'grid','gridAutoFlow':'column','gap':'10px','width':'fit-content'}}>
                                 <a href={playLink}>
                                     <img src={googleplay} width="120px"/>
