b15ce7bc6bd3983e51d8ad8defb9db2e3559c15b
Chitwan Bindal <chitwan001@gmail.com>
2022-08-13 22:30:02 +0530

    green footer added

--- client/src/components/WiseStamp/components/Apps.jsx
+++ client/src/components/WiseStamp/components/Apps.jsx
@@ -1,5 +1,5 @@
-import { RightArrowCircle } from "@styled-icons/boxicons-solid";
-import { Mobile } from "@styled-icons/entypo";
+import { QuoteAltLeft, RightArrowCircle } from "@styled-icons/boxicons-solid";
+import { Leaf, Mobile } from "@styled-icons/entypo";
 import { InkStroke } from "@styled-icons/fluentui-system-regular";
 import { BurstSale } from "@styled-icons/foundation";
 import { Gavel } from "@styled-icons/material-rounded";
@@ -11,20 +11,32 @@ export default function Apps(){
     const [visible, setVisible] = useState(false);
     return(
         <>
-        <div className="grid grid-rows-[auto_auto]">
+        <div className="grid grid-rows-[auto_auto] gap-[10px]">
             <div className="grid grid-rows-[1fr_4fr] gap-[5px]">
                 <div className="self-center pl-[5px] font-bold tracking-wider">ENHANCE YOUR SIGNATURE</div>
-                <div className="grid grid-cols-[1fr_1fr] gap-[5px] h-fit">
-                    <div className="cursor-pointer grid grid-flow-col place-self-center h-[50px] w-[90%] shadow-[rgba(0,_0,_0,_0.02)_0px_1px_3px_0px,_rgba(27,_31,_35,_0.15)_0px_0px_0px_1px] rounded bg-[#f8f9fb]" onClick={() => {setVisible(true);setwhichDrawer('signoff')}}>
+                <div className="grid grid-cols-[1fr_1fr] gap-[10px] h-fit px-[10px]">
+                    <div className="cursor-pointer grid grid-flow-col place-self-center h-[50px] w-full shadow-[rgba(0,_0,_0,_0.02)_0px_1px_3px_0px,_rgba(27,_31,_35,_0.15)_0px_0px_0px_1px] rounded bg-[#f8f9fb]" onClick={() => {setVisible(true);setwhichDrawer('signoff')}}>
                         <div className="grid grid-cols-[auto_1fr] place-self-center gap-[8px]">
                             <div><InkStroke className="w-[25px] text-green-500 font-bold"/></div>
                             <div className="pl-[5px] font-bold">Styled Signoff</div>
                         </div>
                     </div>
-                    <div className="cursor-pointer grid grid-flow-col place-self-center h-[50px] w-[90%] shadow-[rgba(0,_0,_0,_0.02)_0px_1px_3px_0px,_rgba(27,_31,_35,_0.15)_0px_0px_0px_1px] rounded bg-[#f8f9fb]" onClick={() => {setVisible(true);setwhichDrawer('disclaimer')}}>
+                    <div className="cursor-pointer grid grid-flow-col place-self-center h-[50px] w-full shadow-[rgba(0,_0,_0,_0.02)_0px_1px_3px_0px,_rgba(27,_31,_35,_0.15)_0px_0px_0px_1px] rounded bg-[#f8f9fb]" onClick={() => {setVisible(true);setwhichDrawer('disclaimer')}}>
                         <div className="grid grid-cols-[auto_1fr] place-self-center gap-[8px]">
                             <div><Gavel className="w-[25px] text-blue-700 font-bold"/></div>
                             <div className="pl-[5px] font-bold">Disclaimer</div>
+                        </div>
+                    </div>
+                    <div className="cursor-pointer grid grid-flow-col place-self-center h-[50px] w-full shadow-[rgba(0,_0,_0,_0.02)_0px_1px_3px_0px,_rgba(27,_31,_35,_0.15)_0px_0px_0px_1px] rounded bg-[#f8f9fb]" onClick={() => {setVisible(true);setwhichDrawer('quote')}}>
+                        <div className="grid grid-cols-[auto_1fr] place-self-center gap-[8px]">
+                            <div><QuoteAltLeft className="w-[25px] text-green-700 font-bold"/></div>
+                            <div className="pl-[5px] font-bold">Quote</div>
+                        </div>
+                    </div>
+                    <div className="cursor-pointer grid grid-flow-col place-self-center h-[50px] w-full shadow-[rgba(0,_0,_0,_0.02)_0px_1px_3px_0px,_rgba(27,_31,_35,_0.15)_0px_0px_0px_1px] rounded bg-[#f8f9fb]" onClick={() => {setVisible(true);setwhichDrawer('greenfooter')}}>
+                        <div className="grid grid-cols-[auto_1fr] place-self-center gap-[8px]">
+                            <div><Leaf className="w-[25px] text-green-700 font-bold"/></div>
+                            <div className="pl-[5px] font-bold">Grren Footer</div>
                         </div>
                     </div>
                 </div>
--- client/src/components/WiseStamp/components/Apps/AppDrawer.jsx
+++ client/src/components/WiseStamp/components/Apps/AppDrawer.jsx
@@ -3,6 +3,8 @@ import CustomButton from "./CustomButton";
 import Disclaimer from "./Disclaimer";
 import DownloadApp from "./DownloadApp";
 import SignOff from "./SignOff";
+import Quote from "./Quote";
+import GreenFooter from "./GreenFooter";
 export default function AppDrawer({whichApp,visible,setVisible}){
     const onClose = () => {
         setVisible(false);
@@ -16,22 +18,36 @@ export default function AppDrawer({whichApp,visible,setVisible}){
     }
     else if(whichApp==='custombutton'){
         return(
-            <Drawer width='80%' title="Signoff" placement="right" onClose={onClose} visible={visible}>
+            <Drawer width='80%' title="Custom Button" placement="right" onClose={onClose} visible={visible}>
                 <CustomButton/>
             </Drawer>
         )
     }
     else if(whichApp==='downloadapp'){
         return(
-            <Drawer width='80%' title="Signoff" placement="right" onClose={onClose} visible={visible}>
+            <Drawer width='80%' title="Download App" placement="right" onClose={onClose} visible={visible}>
                 <DownloadApp/>
             </Drawer>
         )
     }
     else if(whichApp==='disclaimer'){
         return(
-            <Drawer width='80%' title="Signoff" placement="right" onClose={onClose} visible={visible}>
+            <Drawer width='80%' title="Disclaimer" placement="right" onClose={onClose} visible={visible}>
                 <Disclaimer/>
+            </Drawer>
+        )
+    }
+    else if(whichApp==='quote'){
+        return(
+            <Drawer width='80%' title="Quote" placement="right" onClose={onClose} visible={visible}>
+                <Quote/>
+            </Drawer>
+        )
+    }
+    else if(whichApp==='greenfooter'){
+        return(
+            <Drawer width='80%' title="Quote" placement="right" onClose={onClose} visible={visible}>
+                <GreenFooter/>
             </Drawer>
         )
     }
